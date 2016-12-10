9457cd866ce6946af1394a05e92db7f4f7df1274
Samuele Mega <samuelemega@gmail.com>
2016-12-10 10:44:49 +0100

    Input e output aggiornato

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main () {
+    ifstream input;
+    ofstream output;
+
+    input.open("input.txt");
+    output.open("output.txt");
+
+    input.close();
+    output.close();
+
     return 0;
 }
